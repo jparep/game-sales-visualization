28c94c8fc5e5831cbea8ef33ba51ded026df00d5
jparep <parep@hawaii.edu>
2024-12-14 20:36:19 -0700

    Visualiaze sum of the sales by year

--- script.R
+++ script.R
@@ -25,3 +25,30 @@ games$Year <- factor(games$Year)
 games <- games[,2:11]
 
 head(games)
+
+sumofsales <- games %>%
+  group_by(Year) %>%
+  summarise(sum_global_sales = sum(Global_Sales),sum_others_sales = sum(Other_Sales),
+            sum_jb_sales = sum(JP_Sales),sum_eu_sales = sum(EU_Sales),
+            sum_na_sales = sum(NA_Sales),.groups = 'drop')
+
+colors <- c("Global Sales"="red", "North America Sales"="blue", "Europe Sales"="green", "Japan Sales"="orange",
+            "The Rest of the World"="yellow")
+options(repr.plot.width = 16, repr.plot.height = 8)
+ggplot(data=sumofsales, aes(x= Year)) +
+  geom_line(aes(y= sum_global_sales,group=1,color="Global Sales"),linetype = "dashed")+
+  geom_line(aes(y= sum_na_sales,group=1,color="North America Sales"),linetype = "dashed")+
+  geom_line(aes(y= sum_eu_sales,group=1,color="Europe Sales"),linetype = "dashed")+
+  geom_line(aes(y= sum_jb_sales,group=1,color="Japan Sales"),linetype = "dashed")+
+  geom_line(aes(y= sum_others_sales,group=1,color="The Rest of the World"),linetype = "dashed")+
+  geom_point(aes(y= sum_global_sales)) +
+  geom_point(aes(y= sum_na_sales)) +
+  geom_point(aes(y= sum_eu_sales)) +
+  geom_point(aes(y= sum_jb_sales)) +
+  geom_point(aes(y= sum_others_sales)) +
+  scale_color_manual(name="Sales",values = colors)+
+  ggtitle("Sum of Global Sales by Year") +
+  xlab("Years") +
+  ylab("in millions") +
+  theme_stata()+
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),legend.position="top")
